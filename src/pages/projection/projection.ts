4b57857a5a20191c508b6441119dd733474f7994
coucou <coucou@gmail.com>
2017-07-06 15:08:27 +0200

    test

--- src/pages/projection/projection.ts
+++ src/pages/projection/projection.ts
@@ -1,6 +1,8 @@
 import { Component,ViewChild } from '@angular/core';
 import { NavController, MenuController } from 'ionic-angular';
 import { App, ViewController } from 'ionic-angular';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
     selector: 'coaching-app',
@@ -10,21 +12,21 @@ import { App, ViewController } from 'ionic-angular';
 export class Projection{
 
     mesCoachings: any[];
-    
+    bgImage:any;
 
-    constructor(public navCtrl: NavController, public menu: MenuController,public appCtrl: App) {
+    constructor(public navCtrl: NavController, public menu: MenuController,public appCtrl: App,private sanitizer: DomSanitizer) {
     }
 
     ngOnInit() {
         this.mesCoachings = [
             { img: "vacances.jpg",date: "01/09/2017", titre: "Voyage au Pérou", 
-            evenement: "100" , icone: "football", sourceimg : "google.png", color : "#111111"},  
+            evenement: "100" , icone: "football", sourceimg : "google.png", color : "#111111",bgImage : this.sanitizer.bypassSecurityTrustStyle('url(../../assets/img/vacances.jpg')},  
 
             { img: "maison.jpg",date: "10/09/2017", titre: "Anniversaire de Matéo", 
-            evenement: "500", icone: "home", sourceimg : "facebook.png" , color : "#111111"} ,    
+            evenement: "500", icone: "home", sourceimg : "facebook.png" , color : "#111111",bgImage : this.sanitizer.bypassSecurityTrustStyle('url(../../assets/img/maison.jpg')} ,    
 
             { img: "voiture.png",date: "09/09/2017", titre: "Contrôle technique", 
-            evenement: "50", icone: "contacts", sourceimg : "google.png" , color : "#111111"}
+            evenement: "50", icone: "contacts", sourceimg : "google.png" , color : "#111111",bgImage : this.sanitizer.bypassSecurityTrustStyle('url(../../assets/img/voiture.jpg')}
 
                
         ]
